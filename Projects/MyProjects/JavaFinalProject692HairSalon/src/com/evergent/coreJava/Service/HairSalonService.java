35f1b9db6e0b27611cb560ba57eaa5b5fbee6380
mbhanusagar04 <104252276+mbhanusagar04@users.noreply.github.com>
2024-09-19 11:14:26 +0530

    Add files via upload

---
+++ Projects/MyProjects/JavaFinalProject692HairSalon/src/com/evergent/coreJava/Service/HairSalonService.java
@@ -0,0 +1,32 @@
+package com.evergent.coreJava.Service;
+
+public class HairSalonService {
+    HairSalonDAO salonDAO = new HairSalonDAO();
+
+    public int addService(int serviceId, String serviceName, double price) {
+        HairSalonBean service = new HairSalonBean();
+        service.setServiceId(serviceId);
+        service.setServiceName(serviceName);
+        service.setPrice(price);
+        return salonDAO.addService(service);
+    }
+
+    public String viewService(int serviceId) {
+        return salonDAO.getService(serviceId);
+    }
+
+    public int bookAppointment(int appointmentId, String customerName, String serviceName, String appointmentDate) {
+        AppointmentBean appointment = new AppointmentBean();
+        appointment.setAppointmentId(appointmentId);
+        appointment.setCustomerName(customerName);
+        appointment.setServiceName(serviceName);
+        appointment.setAppointmentDate(appointmentDate);
+        return salonDAO.bookAppointment(appointment);
+    }
+
+    public String viewAllAppointments() {
+        return salonDAO.getAllAppointments();
+    }
+}
+
+
